5a9b098dee402c949f9ff7d72485af83e226d13f
almar94 <almareshef3@gmail.com>
2020-03-15 20:51:14 +0200

    Task list_DataManager

--- app/src/main/java/com/android/tasks/MainActivity.java
+++ app/src/main/java/com/android/tasks/MainActivity.java
@@ -3,9 +3,12 @@ package com.android.tasks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    final int CODE = 2594;
+    Button addNewTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
