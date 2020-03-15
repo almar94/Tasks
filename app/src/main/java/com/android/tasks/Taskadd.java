a76b86f5976f9245b4eedf723635ce0ebdf6ce54
almar94 <almareshef3@gmail.com>
2020-03-15 19:45:50 +0200

    Task list

---
+++ app/src/main/java/com/android/tasks/Taskadd.java
@@ -0,0 +1,14 @@
+package com.android.tasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Taskadd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taskadd);
+    }
+}
